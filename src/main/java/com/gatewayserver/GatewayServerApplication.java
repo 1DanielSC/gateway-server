919257c84377dd2db0fc9b1af0112a9225d61422
1danielsc <danielscolao@gmail.com>
2022-12-13 16:06:20 -0300

    update

--- src/main/java/com/gatewayserver/GatewayServerApplication.java
+++ src/main/java/com/gatewayserver/GatewayServerApplication.java
@@ -2,7 +2,10 @@ package com.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,5 +14,11 @@ public class GatewayServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
+
+	@LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
 }
