eb725c73ac6205326790333b04171aeb43ad57a2
1danielsc <danielscolao@gmail.com>
2022-12-14 21:30:15 -0300

    feat: zipkin

--- src/main/java/com/gatewayserver/GatewayServerApplication.java
+++ src/main/java/com/gatewayserver/GatewayServerApplication.java
@@ -7,12 +7,19 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import brave.sampler.Sampler;
+
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayServerApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+    @Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 	@LoadBalanced
