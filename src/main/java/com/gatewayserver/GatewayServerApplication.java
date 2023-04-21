c217cd1dfc09fde919a4346381caff0a00a232b7
1danielsc <danielscolao@gmail.com>
2023-04-21 15:46:49 -0300

    build: upgrade Java to 19 and SpringBoot to 3.0.6

--- src/main/java/com/gatewayserver/GatewayServerApplication.java
+++ src/main/java/com/gatewayserver/GatewayServerApplication.java
@@ -3,24 +3,28 @@ package com.gatewayserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import brave.sampler.Sampler;
+//import brave.sampler.Sampler;
 
+//@EnableEurekaClient 
+/*
+* Eureka version 4.0.0 onwards, which is being used in Spring Cloud 2022.0.0, 
+* 	you do not need to explicitly register using the annotation @EnableEurekaClient
+*/
 @SpringBootApplication
-@EnableEurekaClient
 public class GatewayServerApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 
-    @Bean
-	public Sampler defaultSampler(){
-		return Sampler.ALWAYS_SAMPLE;
-	}
+    // @Bean
+	// public Sampler defaultSampler(){
+	// 	return Sampler.ALWAYS_SAMPLE;
+	// }
 
 	@LoadBalanced
     @Bean
